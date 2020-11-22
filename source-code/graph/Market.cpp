ca062837bde473c4fa46e233c3f3603427ca5c35
Egor Bychenok <egorix2000@mail.ru>
2020-11-22 13:12:50 +0300

    fix market constructor

--- source-code/graph/Market.cpp
+++ source-code/graph/Market.cpp
@@ -1,8 +1,8 @@
 #include "Market.h"
 
-Market::Market(int idx, int point_idx,const std::string &name, int product, int productCapacity, int replenishment)
+Market::Market(int idx, int point_idx, const std::string &name, int product, int productCapacity, int replenishment)
                                                                         : Node(idx, point_idx), idx_(idx)
-                                                                        , point_idx_(point_idx_)
+                                                                        , point_idx_(point_idx)
                                                                         , name_(name)
                                                                         , product_(product)
                                                                         , product_capacity_(productCapacity)
