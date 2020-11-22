a9c449074c180a626a5d9a0d8971a3278aa5cca3
benninoda <chestmaikbenninoda@gmail.com>
2020-11-22 18:54:35 +0300

    add_layer10_parser

---
+++ source-code/graph/Train.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liza on 11/22/20.
+//
+
+#include "Train.h"
---
+++ source-code/graph/Train.h
@@ -0,0 +1,16 @@
+//
+// Created by liza on 11/22/20.
+//
+
+#ifndef TEAM3_TRAIN_H
+#define TEAM3_TRAIN_H
+
+
+
+class Train {
+
+};
+
+
+
+#endif //TEAM3_TRAIN_H
