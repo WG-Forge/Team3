3db8ec1b3f6e089628b0ae11683f04d1496639a1
Yagor <egorix2000@mail.ru>
2020-12-25 23:42:04 +0300

    replace dijkstra

--- src/agents/MoveAgent.cpp
+++ src/agents/MoveAgent.cpp
@@ -5,86 +5,13 @@ TrainMovement MoveAgent::move(std::vector<Node*>& graph,
                               Train* train,
                               uint32_t building,
                               Hometown* home) {
-    NextNode newNode;
-    if (train->getGoods() != train->getGoodsCapacity()) {
-        newNode = getNextNode(graph, pointIdxCompression, train,
-                         PathSearchPreferences(0, building, nullptr));
-    } else {
-        newNode = getNextNode(graph, pointIdxCompression, train,
-                         PathSearchPreferences(0, 1, nullptr));
-    }
-    TrainMovement movement = calcMovement(train, newNode.node);
+    Node* newNode = getNextNodeCycles(graph, pointIdxCompression, train,
+                         building, home->getPointIdx());
+    TrainMovement movement = calcMovement(train, newNode);
     if (checkForSelfTrainsCollision(movement, home, train)) {
         return movement;
     } else {
         return TrainMovement(train->getEdge(), 0, train->getPosition(), train->getIdx());
-    }
-}
-
-NextNode MoveAgent::getNextNode(std::vector<Node*>& graph,
-                        const std::map<int32_t, uint32_t>& pointIdxCompression,
-                        Train* train,
-                        PathSearchPreferences prefs) {
-    const Edge* trainEdge = train->getEdge();
-    int32_t s1 = pointIdxCompression.at(trainEdge->getFirstNode()->getPointIdx());
-    int32_t s2 = pointIdxCompression.at(trainEdge->getSecondNode()->getPointIdx());
-
-    std::vector<int32_t> dist(graph.size(), INF);
-    std::vector<int32_t> p(graph.size());
-    std::set < std::pair<int32_t,int32_t> > q;
-
-    dist[s1] = train->getPosition();
-    q.insert (std::make_pair (dist[s1], s1));
-    dist[s2] = trainEdge->getLength() - train->getPosition();
-    q.insert (std::make_pair (dist[s2], s2));
-    int32_t end_v = -1;
-
-    // dijkstra
-    while (!q.empty()) {
-        int32_t v = q.begin()->second;
-        q.erase (q.begin());
-
-        if (checkForDestination(graph[v], prefs)) {
-            end_v = v;
-            break;
-        }
-
-        for (auto edge : graph[v]->getNeighbors()) {
-            int32_t toOrigIdx = pointIdxCompression.at(edge->getFirstNode()->getPointIdx()) != v
-                     ? edge->getFirstNode()->getPointIdx()
-                     : edge->getSecondNode()->getPointIdx();
-            int32_t to = pointIdxCompression.at(toOrigIdx);
-            if (checkForTransit(graph[to], prefs)) {
-                int32_t len = edge->getLength();
-                if (dist[v] + len < dist[to]) {
-                    q.erase(std::make_pair(dist[to], to));
-                    dist[to] = dist[v] + len;
-                    p[to] = v;
-                    q.insert(std::make_pair(dist[to], to));
-                }
-            }
-        }
-    }
-
-    // path recovery
-    if (end_v == -1) {
-        return NextNode(nullptr, 0);
-    }
-    int32_t totalLength = dist[end_v];
-    int32_t prev = -1;
-    int32_t v = end_v;
-    while (v != s1 && v != s2) {
-        prev = v;
-        v=p[v];
-    }
-    if (dist[v] == 0) {
-        if (prev != -1) {
-            return NextNode(graph[prev], totalLength);
-        } else {
-            return NextNode(nullptr, 0);;
-        }
-    } else {
-        return NextNode(graph[v], totalLength);
     }
 }
 
@@ -132,42 +59,6 @@ TrainMovement MoveAgent::calcMovement(Train *train, Node* nextNode) {
     newPosition += speed;
 
     return TrainMovement(movementEdge, speed, newPosition, train->getIdx());
-}
-
-bool MoveAgent::checkForDestination(Node *node, PathSearchPreferences prefs) {
-    bool isValidDestination = false;
-    if (prefs.isMovingToSpecificNode) {
-        isValidDestination = node->getPointIdx() == prefs.destination->getPointIdx();
-    } else {
-        if (node->getType() == prefs.buildingType) {
-            if (prefs.buildingType == 1) {
-                isValidDestination = ((Town *) node)->isMine();
-            } else if (prefs.buildingType == 2) {
-                isValidDestination = (((Market *) node)->getProduct() > 0)
-                        || (((Market *) node)->getReplenishment() > 0);
-            } else if (prefs.buildingType == 3) {
-                isValidDestination = (((Storage *) node)->getArmor() > 0)
-                        || (((Storage *) node)->getReplenishment() > 0);
-            }
-        }
-    }
-    return isValidDestination;
-}
-
-bool MoveAgent::checkForTransit(Node *node, PathSearchPreferences prefs) {
-    bool isValidTransit = true;
-    uint32_t destinationBuildingType = prefs.buildingType;
-    if (prefs.isMovingToSpecificNode) {
-        destinationBuildingType = prefs.destination->getType();
-    }
-    if (destinationBuildingType == 2 && node->getType() == 3) {
-        isValidTransit = (((Storage *) node)->getArmor() == 0)
-                && (((Storage *) node)->getReplenishment() == 0);
-    } else if (destinationBuildingType == 3 && node->getType() == 2) {
-        isValidTransit = (((Market *) node)->getProduct() == 0)
-                && (((Market *) node)->getReplenishment() == 0);
-    }
-    return isValidTransit;
 }
 
 bool MoveAgent::checkForSelfTrainsCollision(TrainMovement movement,
@@ -234,15 +125,61 @@ std::vector<TrainMovement> MoveAgent::moveAll(std::vector<Node*>& graph,
     return movements;
 }
 
+Node* MoveAgent::getNextNodeCycles(std::vector<Node*>& graph,
+                                   const std::map<int32_t, uint32_t> &pointIdxCompression,
+                                   Train *train, int32_t buildingType, uint32_t homeIdx) {
+    const std::vector<int> *cycle;
+    if (train->getGoods() > 0 && train->getGoods() < train->getGoodsCapacity()) {
+        return nullptr;
+    }
+    if (buildingType == 2) {
+        for (int i = 0; i < marketCycles.size(); i++) {
+            if (marketCycles[i][0] == homeIdx) {
+                cycle = &marketCycles[i];
+            }
+        }
+    } else {
+        for (int i = 0; i < storageCycles.size(); i++) {
+            if (storageCycles[i][0] == homeIdx) {
+                cycle = &storageCycles[i];
+            }
+        }
+    }
+
+    int firstIdx = train->getEdge()->getFirstNode()->getPointIdx();
+    int secondIdx = train->getEdge()->getSecondNode()->getPointIdx();
+
+    int nextNodeIdx;
+    for (int i = 0; i < cycle->size(); i++) {
+        if (cycle->at(i) == firstIdx) {
+            int nextPosition = getNextIndex(i, cycle->size());
+            if (cycle->at(nextPosition) == secondIdx) {
+                nextNodeIdx = secondIdx;
+                if (train->getPosition() == train->getEdge()->getLength()) {
+                    nextNodeIdx = cycle->at(getNextIndex(nextPosition, cycle->size()));
+                }
+            } else {
+                nextNodeIdx = firstIdx;
+                if (train->getPosition() == 0) {
+                    nextNodeIdx = cycle->at(getNextIndex(i, cycle->size()));
+                }
+            }
+            return graph.at(pointIdxCompression.at(nextNodeIdx));
+        }
+    }
+    return nullptr;
+}
+
+int MoveAgent::getNextIndex(int currentIndex, int vectorSize) {
+    int nextPosition = currentIndex+1;
+    if (nextPosition >= vectorSize) {
+        nextPosition = 0;
+    }
+    return nextPosition;
+}
+
 TrainMovement::TrainMovement(const Edge* line, int32_t speed, uint32_t newPos, uint32_t trainIdx)
                                                             : line(line)
                                                             , speed(speed)
                                                             , trainIdx(trainIdx)
                                                             , newPosition(newPos) {}
-
-PathSearchPreferences::PathSearchPreferences(bool isMovingToSpecificNode, int32_t buildingType, Node *destination)
-        : isMovingToSpecificNode(isMovingToSpecificNode), buildingType(buildingType), destination(destination) {}
-
-NextNode::NextNode(Node *node, uint32_t totalPathLength) : node(node), totalPathLength(totalPathLength) {}
-
-NextNode::NextNode() : node(nullptr), totalPathLength(0) {}
