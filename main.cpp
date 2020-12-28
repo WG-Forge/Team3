1f64295083d84b7f82f29a2f30e569f9d91dda58
Yagor <egorix2000@mail.ru>
2020-12-28 16:58:59 +0300

    set game 4 players

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ int main() {
     Observer observer;
 
     auto windowConfig = observer.launchGame(defines::player_info::PLAYER_NAME.data(),
-                                            defines::player_info::PASSWORD.data(), "", 500, 1);
+                                            defines::player_info::PASSWORD.data(),
+                                            "Grand Final", 500, 4);
     observer.startGame(windowConfig);
     observer.endGame();
 
--- src/render-components/ResourceManager.h
+++ src/render-components/ResourceManager.h
@@ -10,6 +10,7 @@ private:
     std::map<std::string, std::unique_ptr<sf::Texture>> assets_;
     std::map<std::string, std::unique_ptr<sf::Font>> fonts_;
     const std::string PREFIX = "../";
+    //const std::string PREFIX = "../../";
 public:
     sf::Texture* getOrLoadAsset(const std::string& path);
     sf::Font* getOrLoadFont(const std::string& path);
